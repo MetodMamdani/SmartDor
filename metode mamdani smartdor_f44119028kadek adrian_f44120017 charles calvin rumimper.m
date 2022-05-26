1c837157b3339a750dfaf613931a650a65aaab19
MetodMamdani <106319330+MetodMamdani@users.noreply.github.com>
2022-05-26 20:16:39 +0700

    Add files via upload

---
+++ metode mamdani smartdor_f44119028kadek adrian_f44120017 charles calvin rumimper.m
@@ -0,0 +1,119 @@
+{
+ "cells": [
+  {
+   "cell_type": "smartdor",
+   "id": "11fff17c",
+   "metadata": {},
+   "source": [
+    "---\n",
+    "FUZZY MAMDANI\n",
+    "---"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "b2da2107",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "# Mengimpor Library \n",
+    "import numpy as np\n",
+    "import skfuzzy as fuzz\n",
+    "import matplotlib.pyplot as plt"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 2,
+   "id": "74af7be1",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 3,
+   "id": "d11a5a80",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "# Menentukan Range Himpunan Fuzzy\n",
+    "\n",
+    "#pintu bergerak\n",
+    "pintu tidak bergerak = fuzz.trapmf(,])\n",
+    "pintu bergerak = fuzz.trapmf(,[)\n",
+    "\n",
+    "#sensor api\n",
+    "menyala = fuzz.trapmf(,)\n",
+    "tidak menyala= fuzz.trapmf(, )\n",
+    "\n",
+    "#sensor gempa\n",
+    "sensor aktiv= fuzz.trapmf(Akses, )\n",
+    "sensor tidak aktiv = fuzz.trapmf (, )\n",
+    "\n",
+    
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 4,
+   "id": "f57982ca",
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "macis": 
+       "<Figure size 576x648 with 4 Axes>"
+      ]
+     },
+     "metadata": {
+      "needs_background": "light"
+     },
+     "output_type": "display_data"
+    }
+   ],
+   "source": [
+    
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 5,
+   "id": "4059a012",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "# Mengimput Nilai Kasus\n",
+    "LGT = 50\n",
+    "LT = 40\n",
+    "ASA = 40"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 6,
+   "id": "861f93b6",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "# Menentukan Rule Base\n",
+    "\"\"\"\n",
+    
+   
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 7,
+   "id": "cae4f2b3",
+   "metadata": {},
+   "outputs": [
+    {
+   
+     ]
+    }
+   ],
+  
